12417dc2b58f1ac115d08a4d85c87364e0054532
Mingsicogi <codedoctor119@gmail.com>
2020-12-27 19:47:32 +0900

    package

--- packagePractice.py
+++ packagePractice.py
@@ -1,5 +1,14 @@
-import travel.thailand
+# from travel import * 
 
-trip_to = travel.thailand.ThailandPackage()
+# trip_to = thailand.ThailandPackage()
+# trip_to.detail()
 
+
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
+
+
+import travel
+
+trip_to = travel.vietnam.VietnamPackage()
 trip_to.detail()
--- travel/vietnam.py
+++ travel/vietnam.py
@@ -1,3 +1,9 @@
 class VietnamPackage:
     def detail(self):
         print("베트남 여향")
+
+
+if __name__ == "__main__":
+    print("직접 import되서 사용됩니다.")
+else:
+    print("외부 모듈로 사용됩니다.")
